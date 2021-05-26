3863a7071bdb357c45ea7372037e5ed466f08f27
Norimar Iguchi <gguchi193@gmail.com>
2021-05-26 18:22:47 +0800

    Fixed ajax method of displaying posts

--- reddit.js
+++ reddit.js
@@ -1,27 +1,26 @@
+var reddit = new XMLHttpRequest();
+var url = "https://www.reddit.com/r/aww.json";
+reddit.onreadystatechange = function(){
+    if(reddit.readyState == 4 && reddit.status == 200){
+        var posts = JSON.parse(this.responseText);
+        display(posts);
+        console.log(JSON.parse(reddit.response).data.children);
+    }
+};
+reddit.open("GET", url , true);
+reddit.send();
+ function display(arr){
 
-    let reddit = new XMLHttpRequest();
+    var post = JSON.parse(JSON.stringify(arr));
+    let container = document.getElementById("feed");
 
-    reddit.onreadystatechange = function(){
+    post.data.children.forEach(element => {
 
-        if(reddit.readyState == 4 && reddit.status == 200){
-
-            document.getElementById("post").innerHTML = reddit.responseText;
-            console.log(reddit.response);
-
-            console.log(JSON.parse(reddit.response).data.children);
-
-        }
-
-    };
-
-    array.forEach(element => {
+        let post = document.createElement('div');
+        post.classList.add("posts");
+        post.innerHTML = '<h4>'+ element.data.title +'</h4>' + '<img src='+element.data.thumbnail+'></img>'
         
+        container.appendChild(post);
+    
     });
-    
-    var posts = reddit.response;
-    var obj = JSON.stringify(posts);
-    
-    console.log(obj);
-    reddit.open("GET", "https://www.reddit.com/r/aww.json");
-    reddit.send();
-
+}
